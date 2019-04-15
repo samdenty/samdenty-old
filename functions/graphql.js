214db4e3c8621af1b1e591dea652d1ac962a7eee
Sam Denty <samddenty+gh@gmail.com>
2019-04-15 09:36:28 +0100

    Spotify lambda

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,6 +1,14 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World',
-  })
-}
+const { getSchema } = require('spotify-graphql')
+const { ApolloServer } = require('apollo-server-lambda')
+
+const schema = getSchema({
+  clientId: null,
+  clientSecret: null,
+  accessToken: process.env.SPOTIFY_TOKEN,
+})
+
+const server = new ApolloServer({
+  schema,
+})
+
+exports.handler = server.createHandler()
