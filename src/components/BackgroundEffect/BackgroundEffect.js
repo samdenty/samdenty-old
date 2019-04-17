543dd2ffadd0fccfc5e40d2c046990fa85e7ca5a
Sam Denty <samddenty+gh@gmail.com>
2019-04-17 02:28:50 +0100

    fix

---
+++ src/components/BackgroundEffect/BackgroundEffect.js
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+import { createBackgroundEffect } from './createBackgroundEffect'
+
+export const StyledCanvas = styled.canvas`
+  position: fixed;
+  z-index: -1;
+  transition: opacity 2s ease, transform 4s ease;
+  opacity: ${({ loaded }) => (loaded ? 1 : 0)};
+  transform: ${({ loaded }) => (loaded ? 'none' : 'scale(0.7)')};
+`
+
+export const BackgroundEffectContext = React.createContext([false, () => {}])
+
+export const BackgroundEffect = () => {
+  const [paused] = React.useContext(BackgroundEffectContext)
+  const [loaded, setLoaded] = React.useState(false)
+  const canvas = React.useRef(null)
+
+  const effectRef = React.useRef(null)
+
+  React.useEffect(() => {
+    const effect = createBackgroundEffect(canvas.current)
+    effectRef.current = effect
+    effect.draw()
+    setLoaded(true)
+    return () => effect.stop()
+  }, [canvas.current])
+
+  React.useEffect(() => {
+    effectRef.current.stop()
+
+    if (paused) {
+      effectRef.current.stop()
+    } else {
+      effectRef.current.start()
+    }
+  }, [effectRef.current, paused])
+
+  return <StyledCanvas loaded={loaded} ref={canvas} />
+}
+
+export const BackgroundEffectProvider = ({ children }) => {
+  const [paused, setPaused] = React.useState(false)
+
+  return (
+    <BackgroundEffectContext.Provider value={[paused, setPaused]}>
+      {children}
+    </BackgroundEffectContext.Provider>
+  )
+}
--- src/components/BackgroundEffect/createBackgroundEffect.js
+++ src/components/BackgroundEffect/createBackgroundEffect.js
@@ -1,7 +1,4 @@
-import * as React from 'react'
-import styled from '@emotion/styled'
-
-const createEffect = canvas => {
+export const createBackgroundEffect = canvas => {
   if (!canvas) return
 
   const isMobile =
@@ -140,7 +137,7 @@ const createEffect = canvas => {
     var time = new Date().getTime()
     for (var e in elements) elements[e].draw(ctx, time)
 
-    if (again) currentFrame = requestAnimationFrame(() => draw())
+    if (again) currentFrame = requestAnimationFrame(() => draw(again))
   }
 
   let resizeTimer
@@ -164,52 +161,4 @@ const createEffect = canvas => {
     stop,
     draw,
   }
-}
-
-export const StyledCanvas = styled.canvas`
-  position: fixed;
-  z-index: -1;
-  transition: opacity 2s ease, transform 4s ease;
-  opacity: ${({ loaded }) => (loaded ? 1 : 0)};
-  transform: ${({ loaded }) => (loaded ? 'none' : 'scale(0.7)')};
-`
-
-export const BackgroundEffectContext = React.createContext([false, () => {}])
-
-export const BackgroundEffect = () => {
-  const [paused] = React.useContext(BackgroundEffectContext)
-  const [loaded, setLoaded] = React.useState(false)
-  const canvas = React.useRef(null)
-
-  const effectRef = React.useRef(null)
-
-  React.useEffect(() => {
-    const effect = createEffect(canvas.current)
-    effectRef.current = effect
-    effect.draw()
-    setLoaded(true)
-    return () => effect.stop()
-  }, [canvas.current])
-
-  React.useEffect(() => {
-    effectRef.current.stop()
-
-    if (paused) {
-      effectRef.current.stop()
-    } else {
-      effectRef.current.start()
-    }
-  }, [effectRef.current, paused])
-
-  return <StyledCanvas loaded={loaded} ref={canvas} />
-}
-
-export const BackgroundEffectProvider = ({ children }) => {
-  const [paused, setPaused] = React.useState(false)
-
-  return (
-    <BackgroundEffectContext.Provider value={[paused, setPaused]}>
-      {children}
-    </BackgroundEffectContext.Provider>
-  )
 }
