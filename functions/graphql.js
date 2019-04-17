997e602d8efaef1da4ad18b620ff5a5083f31bc5
Sam Denty <samddenty+gh@gmail.com>
2019-04-17 16:33:50 +0100

    use spotify refresh token

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,14 +1,43 @@
-const { getSchema } = require('spotify-graphql')
+require('dotenv').config()
+const typeDefs = require('spotify-graphql/dist/lib/schema').default
+const resolverMapBuilder = require('spotify-graphql/dist/lib/resolvers').default
+const SpotifyWebApi = require('spotify-web-api-node')
+const { makeExecutableSchema } = require('graphql-tools')
 const { ApolloServer } = require('apollo-server-lambda')
 
-const schema = getSchema({
+const client = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  accessToken: process.env.SPOTIFY_TOKEN,
+  redirectUri: 'https://samdd.me',
 })
 
 const server = new ApolloServer({
-  schema,
+  schema: makeExecutableSchema({
+    typeDefs,
+    resolvers: resolverMapBuilder(client),
+  }),
 })
 
-exports.handler = server.createHandler()
+const handler = server.createHandler()
+
+// var authorizeURL = client.createAuthorizeURL([
+//   'user-read-recently-played',
+//   'user-top-read',
+//   'user-read-playback-state',
+//   'user-read-currently-playing',
+// ])
+
+// console.log(authorizeURL)
+
+exports.handler = (event, context, callback) => {
+  client.setRefreshToken(process.env.SPOTIFY_REFRESH_TOKEN)
+  client
+    .refreshAccessToken()
+    .then(data => {
+      client.setAccessToken(data.body['access_token'])
+      handler(event, context, callback)
+    })
+    .catch(error => {
+      console.log('error', error)
+    })
+}
